ae1dc9da38064b3960034f364246a1635de74b94
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2022-12-13 18:08:57 +0530

    footer width

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <div className='bg-purple-300 z-1/2 m-0 bottom-0 w-screen mt-5 p-5'>
+        <div className='bg-purple-300   bottom-0 p-5'>
             <p className='text-center font-semibold'>All rights Reserved to Inderjit Shahi</p>
         </div>
     );
