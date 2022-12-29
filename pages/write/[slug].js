808987b5dc58569f58c7a159929277a6cbd44d95
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2022-12-30 02:23:08 +0530

    added quill

--- pages/write/[slug].js
+++ pages/write/[slug].js
@@ -1,22 +1,19 @@
 import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
-import IsblogContext from '../../context/IsblogContext';
 import { db } from '../../firebase';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { useForm } from "react-hook-form";
-import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
-import * as Emoji from "quill-emoji";
+import 'react-quill/dist/quill.snow.css';
+// const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 function Write(props) {
     const router = useRouter();
     const currentUser = router.query.slug;
     const { register, handleSubmit } = useForm();
     const [c, setC] = useState();
-
+    // const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
     const addPostToFireBase = async (data) => {
         console.log(data.body);
         if (currentUser) {
@@ -38,26 +35,25 @@ function Write(props) {
         toolbar: [
             [{ header: '1' }, { header: '2' }, { font: [] }],
             [{ size: [] }],
-             
+
             ['bold', 'italic', 'underline', 'strike', 'blockquote',],
-            [{ 'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],  
+            [{ 'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
             [{ 'background': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
-            ['emoji'], 
             [
                 { list: 'ordered' },
                 { list: 'bullet' },
                 { indent: '-1' },
                 { indent: '+1' },
             ],
-            ['link', 'image'],
+            ['link'],
         ],
         clipboard: {
             // toggle to add extra line breaks when pasting HTML:
             matchVisual: false,
         },
-        "emoji-toolbar": true,
-        "emoji-textarea": true,
-        "emoji-shortname": true,
+        // "emoji-toolbar": true,
+        // "emoji-textarea": true,
+        // "emoji-shortname": true,
     }
     /*
      * Quill editor formats
@@ -74,12 +70,11 @@ function Write(props) {
         'background',
         'strike',
         'blockquote',
-        'emoji',
         'list',
         'bullet',
         'indent',
         'link',
-        'image',
+        // 'image',
     ]
 
 
@@ -137,7 +132,7 @@ function Write(props) {
                             name="content"
                             value={value} onChange={onContentChange}
                             rows={10} {...register("postLength", { required: true, validate: value => value.trim().length >= 1 })} className="w-full py-1.5 px-3 text-gray-700  border border-solid border-gray-300 rounded focus:border-blue-600 focus:outline-none" placeholder='Your Thoughts' /> */}
-                        <ReactQuill formats={formats} modules={modules} theme='snow' onChange={(e) => setC(e)}></ReactQuill>
+                        <ReactQuill modules={modules} formats={formats} onChange={data => setC(data)}  theme="snow"/>
                     </div>
 
                     <input data-mdb-ripple="true" data-mdb-ripple-color="light" type="submit" className="bg-blue-600 text-white border border-solid border-gray-300 mb-2 p-2 cursor-pointer focus:border-blue-600 w-full uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg hover:scale-95 transition duration-150 ease-in-out" />
