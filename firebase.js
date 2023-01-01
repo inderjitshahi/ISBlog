80c9d8a2d07eadf3dd5beef23d697a630974f1ad
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2023-01-02 01:14:19 +0530

    added readme

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 import { GoogleAuthProvider } from "firebase/auth";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,12 +17,12 @@ const firebaseConfig = {
   messagingSenderId: "383070410348",
   appId: "1:383070410348:web:74c8b6e0d7ef12eb05aa49",
   measurementId: "G-QC9L9VB74F"
-};
+}; 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const auth=getAuth(app);
-const provider=new GoogleAuthProvider();
-const db=getFirestore(app);
-export {auth, provider, db} ;
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+export { auth, provider, db };
